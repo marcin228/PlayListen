9476d38643d562da5555acde0eb25df2c801015c
Marcin228 <marcin@goingforit.pl>
2024-05-16 16:01:41 +0200

    Added newly added item highlight feature.

--- src/components/Ads/Ads.tsx
+++ src/components/Ads/Ads.tsx
@@ -1,6 +1,6 @@
 import React from "react"
-import styles from "./Ads.module.css"
-import axios from "axios";
+//import styles from "./Ads.module.css"
+//import axios from "axios";
 
 type AdsProps = {
     children?:React.ReactNode,
--- src/components/PlaylistEditor/PlaylistEditor.tsx
+++ src/components/PlaylistEditor/PlaylistEditor.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useRef } from "react"
 import styles from "../PlaylistEditor/PlaylistEditor.module.css";
 import Positionable from "../../classes/Positionable";
 import { PlaylistEditable } from "../Playlists/Playlists";
@@ -7,7 +7,7 @@ import PlaylistObject from "../../classes/PlaylistObject";
 import PlaylistAdder from "../PlaylistAdder/PlaylistAdder";
 import DispatchActionFactory from "../../classes/DispatchActionFactory";
 import { useGlobalContext } from "../../hooks/useGlobalContext";
-import { motion, AnimatePresence, /* useAnimate */ } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import DeleteSvg from '../../assets/delete.svg';
 import UpSvg from '../../assets/up.svg';
 import DownSvg from '../../assets/down.svg';
@@ -24,7 +24,9 @@ const PlaylistEditor:React.FC<PlaylistEditorProps> = ({ playlist, playlistId, ch
 
     const items:Array<Positionable> = playlist.items;
     const navigate = useNavigate();
-    const { dispatch } = useGlobalContext();
+    const { state, dispatch } = useGlobalContext();
+    const lastPlaylistId = useRef<null | number>(null); //useState<null | number>(null);
+    const lastPlaylistLength = useRef<null | number>(null); //useState<null | number>(null);
     //const [ scope, animate ] = useAnimate();
     
     const onClickTitleHandler = function(e:React.MouseEvent){
@@ -54,7 +56,48 @@ const PlaylistEditor:React.FC<PlaylistEditorProps> = ({ playlist, playlistId, ch
                 dispatch(DispatchActionFactory.movePlaylistDown(parseInt(htmlData.playlistid!)));
         }
     }
-    
+
+    const getItemStyles = function(itemPosition:number):string{
+
+        if(playlistId == undefined)
+            return styles.item;
+
+        const addingToPlaylistWithId:number = parseInt(playlistId);
+
+        if(itemPosition !== state?.playlists![addingToPlaylistWithId].items.length-1)
+            return styles.item;
+
+        if(lastPlaylistId.current == parseInt(playlistId!))
+            if(lastPlaylistLength.current == state?.playlists![addingToPlaylistWithId].items.length)
+                return styles.item;
+
+        if(lastPlaylistId.current !== null && lastPlaylistLength.current !== null){
+
+            if(lastPlaylistId.current == addingToPlaylistWithId){
+                if(lastPlaylistLength.current + 1 == state?.playlists![addingToPlaylistWithId].items.length){
+                    
+                    lastPlaylistLength.current = state?.playlists![addingToPlaylistWithId].items.length;
+                    return `${styles.item} ${styles.item___added}`;
+                }
+                else{
+                    lastPlaylistLength.current = state?.playlists![addingToPlaylistWithId].items.length;
+                }
+            }
+            else{
+                lastPlaylistId.current = addingToPlaylistWithId;
+                lastPlaylistLength.current = state?.playlists![addingToPlaylistWithId].items.length;
+            }
+        }
+        else{
+
+            console.log('vars unset')
+            lastPlaylistId.current = addingToPlaylistWithId;
+            lastPlaylistLength.current = state?.playlists![addingToPlaylistWithId].items.length;
+        }
+
+        return styles.item;
+    }
+
     if(!Array.isArray(items))
         return (<></>);
 
@@ -63,25 +106,27 @@ const PlaylistEditor:React.FC<PlaylistEditorProps> = ({ playlist, playlistId, ch
     <motion.div className={styles.playlists}>
         <PlaylistAdder playlist={playlist} playlistId={playlistId} key="playlistAdder"></PlaylistAdder>
         <AnimatePresence>
-        { items && items.map(item => <motion.div initial={{opacity: 0}} animate={{opacity:1}} key={ item.title } className={ styles.item }>
+        { items && items.map(item => <motion.div initial={{opacity: 0}} animate={{opacity:1}}
+                key={ item.title } className={ getItemStyles(item.position) }>
                 <div className={`${styles.itemTitle} ${((item as PlaylistObject).id !== undefined) ? styles.itemTitleCursor : ''}`}
                  {...(((item as PlaylistObject).id !== undefined) && { onClick: onClickTitleHandler })} 
                  data-id={(item as PlaylistObject).id}>{(item as PlaylistObject).id} {item.title}</div>
                 
-                <div data-identifier="itemDelete" className={ styles.itemDelete } data-position={item.position} data-playlistid={playlist.root?.id} onClick={onClickRemoveHandler}>
+                <div data-identifier="itemDelete" className={ styles.itemDelete } data-position={item.position}
+                data-playlistid={playlist.root?.id} onClick={onClickRemoveHandler}>
                     <ReactSVG src={DeleteSvg} className="deleteSvg" />delete
                 </div>
                 
                 <div data-identifier="itemUp" className={ styles.itemUp } data-direction={"up"} data-position={item.position}
                 data-playlistid={(playlistId) ? playlistId : (item as PlaylistObject).id} 
-                data-isPlaylist={ ((item as PlaylistObject).items !== undefined) }
+                data-isplaylist={ ((item as PlaylistObject).items !== undefined) }
                 onClick={ onClickPositionChangeHandler }>
                     <ReactSVG src={UpSvg} className="upSvg" />up
                 </div>
                 
                 <div data-identifier="itemDown" className={ styles.itemDown } data-direction={"down"} data-position={item.position}
                 data-playlistid={(playlistId) ? playlistId : (item as PlaylistObject).id} 
-                data-isPlaylist={ ((item as PlaylistObject).items !== undefined) }
+                data-isplaylist={ ((item as PlaylistObject).items !== undefined) }
                 onClick={ onClickPositionChangeHandler }>
                     <ReactSVG src={DownSvg} className="downSvg" />down
                 </div>
